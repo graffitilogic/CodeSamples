39c9fff281a623bc81d7442c1ea9a22fc2dc224c
Bill Dodd <bill.dodd@graffitilogic.com>
2024-06-09 21:22:30 -0500

    remove maui

--- bowling-maui/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace bowling;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
