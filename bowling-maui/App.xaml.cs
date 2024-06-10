05a442a1497c429394dd4289c30823bd71071292
Bill Dodd <bill.dodd@graffitilogic.com>
2024-06-09 21:22:02 -0500

    startup project

---
+++ bowling-maui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace bowling;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ bowling-maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace bowling;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
